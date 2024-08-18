b26d17ce65f2d807b24b05801bebb647ffd78e1b
Anton Stanev <antoshef21@gmail.com>
2024-08-18 08:36:52 +0300

    add dev

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Forgot React
+          Learn React
         </a>
       </header>
     </div>
